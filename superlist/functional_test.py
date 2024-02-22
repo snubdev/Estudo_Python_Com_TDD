7b652b63a71a8aec9476203cf82654677a8cc649
Maicon Leandro <snubhousedev@gmail.com>
2024-02-21 21:44:41 -0300

    Implementando novos teste no functional_test.py

--- superlist/functional_test.py
+++ superlist/functional_test.py
@@ -2,11 +2,41 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+import unittest
 
-servico = Service(ChromeDriverManager().install())
-browser = webdriver.Chrome(service=servico)
+# servico = Service(ChromeDriverManager().install())
+# browser = webdriver.Chrome(service=servico)
+#
+# browser.get('http://localhost:8000')
+# sleep(10)
+#
+# assert 'To-Do' in browser.title
+#
+# browser.quit()
 
-browser.get('http://localhost:8000')
-sleep(10)
 
-#assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.servico = Service(ChromeDriverManager().install())
+        self.browser = webdriver.Chrome(service=self.servico)
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar de uma nova aplicação online insteressante
+        # para lista de tarefas. Ela decide verificar sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # Ela percebe que o título da página e o cabeçalho mencionam
+        # lista de tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Ele é convidade a inserir um item de  tarefa imediatamente
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
